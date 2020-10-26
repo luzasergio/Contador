afc27e137ba0ba20966e246b0d32befb8c5a0c2d
sergioluza <luzasergio@gmail.com>
2020-10-26 09:51:45 -0300

    Creación de los botones y texto plain text dentro de un linearLayout Modificación del id de los elementos View (texto y botones)

--- app/src/main/java/com/example/contador/MainActivity.java
+++ app/src/main/java/com/example/contador/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.contador;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
+//public class MainActivity extends AppCompatActivity {
+//Modifico la línea para que la clase herede directamente de Activity, no veo bien cuál es la diferencia en hacer esto
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
